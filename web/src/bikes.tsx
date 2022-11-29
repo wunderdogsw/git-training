d9f356ed9626fae3845e22c5822b9fb873d6c021
Henri Kellock <henri.kellock@wunderdog.fi>
2022-11-29 18:40:52 +0200

    Fix bike status displaying in the ui

--- web/src/bikes.tsx
+++ web/src/bikes.tsx
@@ -14,13 +14,23 @@ type Bike = {
   status: BikeStatus;
 };
 
+const bikeStatusToDisplayValueMap: Record<BikeStatus, string> = {
+  [BikeStatus.Sold]: "Sold",
+  [BikeStatus.Stolen]: "Stolen",
+  [BikeStatus.PartiallyStolen]: "PartiallyStolen",
+  [BikeStatus.NotYetStolen]: "NotYetStolen",
+  [BikeStatus.NotYetBought]: "NotYetBought",
+};
+
 const Bikes: React.FC = () => {
   const bikes: Bike[] = []; // Get bikes from api
 
   return (
     <ul>
       {bikes.map((bike) => (
-        <li>{bike.brand} | {bike.status}</li>
+        <li>
+          {bike.brand} | {bikeStatusToDisplayValueMap[bike.status]}
+        </li>
       ))}
     </ul>
   );
