37ba0098a6b7484da1bf6c3459765a24b1139d7c
Henri Kellock <henri.kellock@wunderdog.fi>
2022-11-29 18:17:01 +0200

    Add bikes api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,11 @@
 // This file contains the api
 
+import { getBikes } from "./bikes";
+
 const api = () => {
+  const handlers: (() => unknown)[] = [];
+  handlers.push(getBikes);
+
   console.log("The api is running...");
 };
 
