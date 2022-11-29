98f84b5080d7563bedfdbf16239c9cde73786702
Henri Kellock <henri.kellock@wunderdog.fi>
2022-11-29 18:27:12 +0200

    Add bike api

--- api/src/bikes.ts
+++ api/src/bikes.ts
@@ -6,14 +6,20 @@ enum BikeStatus {
   NotYetBought,
 }
 
-const getBikes = () => {
-  return [
-    { id: "a", brand: "Rose", status: BikeStatus.Sold },
-    { id: "b", brand: "Canyon", status: BikeStatus.Sold },
-    { id: "c", brand: "Focus", status: BikeStatus.Stolen },
-    { id: "d", brand: "Focus", status: BikeStatus.PartiallyStolen },
-    { id: "e", brand: "Trek", status: BikeStatus.NotYetBought },
-  ];
+const bikes = [
+  { id: "a", brand: "Rose", status: BikeStatus.Sold },
+  { id: "b", brand: "Canyon", status: BikeStatus.Sold },
+  { id: "c", brand: "Focus", status: BikeStatus.Stolen },
+  { id: "d", brand: "Focus", status: BikeStatus.PartiallyStolen },
+  { id: "e", brand: "Trek", status: BikeStatus.NotYetBought },
+];
+
+const getBike = (id: string) => {
+  return bikes.find((bike) => bike.id === id);
 };
 
-export { getBikes };
+const getBikes = () => {
+  return bikes;
+};
+
+export { getBike, getBikes };
